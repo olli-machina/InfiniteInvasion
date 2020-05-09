6efa221e9ff640210f8791ff2424f86d79da5256
Carter Ivancic <carter.ivancic@mymail.champlain.edu>
2020-05-09 23:57:38 +0000

    Linking the Start Screen to the other scenes

---
+++ Assets/Scripts/SceneSwitchScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void LoadHelp()
+    {
+        SceneManager.LoadScene("HelpScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
