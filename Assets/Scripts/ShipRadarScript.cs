4da53841b1ffd5aa9e9a140b9762c32ec85772a3
Carter Ivancic <carter.ivancic@mymail.champlain.edu>
2020-05-10 03:53:33 +0000

    Adding a delay to S.O.S. message

--- Assets/Scripts/ShipRadarScript.cs
+++ Assets/Scripts/ShipRadarScript.cs
@@ -6,17 +6,18 @@ public class ShipRadarScript : MonoBehaviour
 {
     public int threatLevel;
     public GameObject radioBall, messageBorder, message;
+    private bool messageReady;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        messageReady = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (threatLevel >= 5)
+        if (threatLevel >= 5 && messageReady)
         {
             StartCoroutine(MessageIncoming());
         }
@@ -31,6 +32,14 @@ public class ShipRadarScript : MonoBehaviour
         radioBall.GetComponent<RadioBallScript>().EndMessage();
         messageBorder.SetActive(false);
         message.SetActive(false);
+        StartCoroutine(StartCooldown());
+    }
+
+    IEnumerator StartCooldown()
+    {
+        messageReady = false;
+        yield return new WaitForSeconds(15f);
+        messageReady = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
